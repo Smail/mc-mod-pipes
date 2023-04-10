8a2c4de3692ca3feda183c79f16c6a4ef79e4e49
Smail <44433524+Smail@users.noreply.github.com>
2023-04-11 00:02:44 +0200

    refactor: Remove function use

--- src/main/java/com/de/smail/examplemod/blocks/TransportPipeBlock.java
+++ src/main/java/com/de/smail/examplemod/blocks/TransportPipeBlock.java
@@ -2,9 +2,6 @@ package com.de.smail.examplemod.blocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -13,7 +10,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -377,31 +373,6 @@ public class TransportPipeBlock extends Block {
                 .add(IS_CONNECTED_WEST)
                 .add(IS_CONNECTED_ABOVE)
                 .add(IS_CONNECTED_BELOW);
-    }
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public @NotNull InteractionResult use(@NotNull BlockState blockState, Level level, @NotNull BlockPos blockPos,
-                                          @NotNull Player player, @NotNull InteractionHand hand,
-                                          @NotNull BlockHitResult blockHitResult) {
-        if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND) {
-//            System.err.println("Rotate");
-//
-//            blockState = blockState.setValue(FACING, switch (blockState.getValue(FACING)) {
-//                case UP -> Direction.DOWN;
-//                case DOWN -> Direction.NORTH;
-//                case NORTH -> Direction.EAST;
-//                case EAST -> Direction.SOUTH;
-//                case SOUTH -> Direction.WEST;
-//                case WEST -> Direction.UP;
-//            });
-
-            System.out.println(getState(blockState));
-//            level.setBlock(blockPos, blockState, 2);
-//            level.setBlock(blockPos, this.rotate(blockState, level, blockPos, Rotation.CLOCKWISE_90), 1);
-        }
-
-        return InteractionResult.SUCCESS;
     }
 
     @Override
