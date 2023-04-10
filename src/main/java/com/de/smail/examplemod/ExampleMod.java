2c761afbddf39a489d8869d2d6b7af1c5baef7aa
Smail <44433524+Smail@users.noreply.github.com>
2023-04-10 22:33:54 +0200

    Use BlocksLoader and ItemsLoader classes

--- src/main/java/com/de/smail/examplemod/ExampleMod.java
+++ src/main/java/com/de/smail/examplemod/ExampleMod.java
@@ -1,14 +1,12 @@
 package com.de.smail.examplemod;
 
+import com.de.smail.examplemod.blocks.BlocksLoader;
+import com.de.smail.examplemod.items.ItemsLoader;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -20,23 +18,14 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MOD_ID)
 public class ExampleMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "examplemod";
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    // Directly reference a slf4j logger
+    public static final DeferredRegister<Block> BLOCKS = BlocksLoader.BLOCKS;
+    public static final DeferredRegister<Item> ITEMS = ItemsLoader.ITEMS;
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public ExampleMod() {
